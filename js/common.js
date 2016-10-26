1460d09756e18cd9158945107c36d81afc592ed1
jianhao <jianhao@example.com>
2016-10-26 17:03:37 +0800

    版本一

---
+++ js/common.js
@@ -0,0 +1,23 @@
+﻿var _w = document.documentElement.clientWidth || document.body.clientWidth,
+	_h = document.documentElement.clientHeight || document.body.clientHeight,
+    pageW = 750;
+var screenFun = function(){
+    if (_w < _h) {
+        pageW = 750;
+        document.documentElement.style.fontSize = _w/pageW*20+'px';
+        document.body.className = 'vertical';
+    } else{
+        pageW = 1334;
+        document.documentElement.style.fontSize = _w/pageW*20+'px';
+        document.body.className = 'level';
+    }
+}
+document.addEventListener("DOMContentLoaded",function(e){
+	screenFun();
+});
+window.onresize = function(){
+	_w = document.documentElement.clientWidth || document.body.clientWidth,
+	_h = document.documentElement.clientHeight || document.body.clientHeight;
+    screenFun();
+};
+
---
+++ js/sort.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function(){
+    if (_w < _h) {
+       $('.page-nav ul').css('width',$('.page-nav ul').find('li').length*7.5+'rem'); 
+    } else{
+        $('.page-nav ul').css('width',$('.page-nav ul').find('li').length*12+'rem'); 
+    }
+    $(window).resize(function(){
+        if (_w < _h) {
+           $('.page-nav ul').css('width',$('.page-nav ul').find('li').length*7.5+'rem'); 
+        } else{
+            $('.page-nav ul').css('width',$('.page-nav ul').find('li').length*12+'rem');
+        }
+    });
+});
